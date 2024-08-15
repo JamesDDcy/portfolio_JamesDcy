1b7ccc518ff69029a0fd17e6502dbef8eb7ce65c
James Ducay <jpducay@up.edu.ph>
2024-08-16 00:13:07 +0800

    style: project title color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,6 @@ module.exports = {
       }
     },
   },
-  safelist: [
-    'text-green-400',
-    'hover:text-green-600',
-    'text-red-400',
-    'hover:text-red-600',
-    'text-cyan-400',
-    'hover:text-cyan-600',
-    'text-amber-400',
-    'hover:text-amber-600',
-    'text-fuchsia-400',
-    'hover:text-fuchsia-600',
-  ],
   plugins: [],
 }
 
