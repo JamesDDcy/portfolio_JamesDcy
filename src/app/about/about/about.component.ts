d05b17ff7c453dc41a94f6816a27d213e96dc064
James Ducay <jpducay@up.edu.ph>
2024-08-22 00:29:05 +0800

    feat: added images for interests

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AboutComponent {
 
