dbf765638e80fd682d8e64e61b4ece0e23b81e19
James Ducay <jpducay@up.edu.ph>
2024-08-24 00:15:11 +0800

    style: final AOS

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import * as AOS from 'aos';
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, PLATFORM_ID } from '@angular/core';
@@ -12,16 +12,17 @@ import { Inject, PLATFORM_ID } from '@angular/core';
   styleUrls: ['./about.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AboutComponent implements OnInit {
-
+export class AboutComponent implements AfterViewInit {
   constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       AOS.init({
         duration: 1200,
         once: true,
+        offset: 0,  // Ensures animations trigger as soon as the element is in the viewport
       });
+      AOS.refresh();
     }
   }
 }
