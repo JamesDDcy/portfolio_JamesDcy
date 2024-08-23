2d59796aef44554bbdca87abc896c653861528ba
James Ducay <jpducay@up.edu.ph>
2024-08-23 23:48:42 +0800

    feature: added test animations on page

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,13 +1,27 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import * as AOS from 'aos';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css',
+  styleUrls: ['./about.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 1200,
+        once: true,
+      });
+    }
+  }
 }
