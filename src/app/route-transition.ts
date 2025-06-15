89a24e96f6a949ab759c504fe87380cb85b9fea7
James Ducay <jpducay@up.edu.ph>
2025-06-16 00:10:47 +0800

    feat: minor edits on transition

--- src/app/route-transition.ts
+++ src/app/route-transition.ts
@@ -4,10 +4,10 @@ export const routeTransition = trigger('routeTransition', [
     // We want to animate transition from any state
     transition('* => *', [
         query(':enter', [
-            style({ opacity: 0, scale: 0.9 }),
+            style({ opacity: 0, scale: 1 }),
         ], { optional: true }),
         query(':leave', [
-            animate('0.25s', style({ opacity: 0, scale: 0.9 })),
+            animate('0.25s', style({ opacity: 0, scale: 1 })),
         ], { optional: true }),
         query(':enter', [
             animate('0.25s', style({ opacity: 1, scale: 1 })),
