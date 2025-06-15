d97b0b740a001ac77b36758d995f20d6e94de53f
James Ducay <jpducay@up.edu.ph>
2025-06-15 23:35:42 +0800

    feat: page transition animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet, RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { AboutComponent } from './about/about/about.component';
 import { ProjectsComponent } from './projects/projects/projects.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { routeTransition } from './route-transition';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,14 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  animations: [
+    routeTransition
+  ]
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(protected route: ActivatedRoute) {
+
+  }
 }
---
+++ src/app/route-transition.ts
@@ -0,0 +1,16 @@
+import { trigger, query, style, animate, transition } from '@angular/animations';
+
+export const routeTransition = trigger('routeTransition', [
+    // We want to animate transition from any state
+    transition('* => *', [
+        query(':enter', [
+            style({ opacity: 0, scale: 0.9 }),
+        ], { optional: true }),
+        query(':leave', [
+            animate('0.25s', style({ opacity: 0, scale: 0.9 })),
+        ], { optional: true }),
+        query(':enter', [
+            animate('0.25s', style({ opacity: 1, scale: 1 })),
+        ], { optional: true }),
+    ])
+])
