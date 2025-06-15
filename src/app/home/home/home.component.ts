6fd2fc5d848b1c064005ffb3ae257dd3be74cedf
James Ducay <jpducay@up.edu.ph>
2025-06-15 23:31:25 +0800

    feat: interactive home background

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
 
