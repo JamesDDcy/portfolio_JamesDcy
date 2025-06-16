69449e13c0e857c3e5c69c2a386e689e5b03aa6e
James Ducay <jpducay@up.edu.ph>
2025-06-16 23:00:20 +0800

    feat: send email via emailjs

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -1,14 +1,17 @@
 import { Component, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import * as AOS from 'aos';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { Inject, PLATFORM_ID } from '@angular/core';
 import { request } from 'http';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -37,5 +40,55 @@ export class AboutComponent implements AfterViewInit {
       };
       update();
     }
+  }
+
+  emailData = {
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  sendEmailModalOpen = false;
+  openModal() { this.sendEmailModalOpen = true; }
+  closeModal() {
+    this.sendEmailModalOpen = false;
+    this.emailStatus = null; // Reset email status when closing the modal
+    this.emailData = {
+      email: '',
+      subject: '',
+      message: ''
+    }; // Reset email data when closing the modal
+  }
+
+
+  validateEmailForm() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return (
+      this.emailData.email &&
+      emailPattern.test(this.emailData.email)
+    );
+  }
+
+  validateSubjectOrMessage(data: string) {
+    return (
+      data && data.length >= 5
+    );
+  }
+
+  emailStatus: 'success' | 'error' | null = null;
+
+  sendEmail() {
+    emailjs.send('service_0pir1td', 'template_mlg9imm', { ...this.emailData }, {
+      publicKey: 'V1ovjeeoPRfAGNba2'
+    }).then(() => {
+      this.emailStatus = 'success';
+      // Optionally close the modal after a short delay
+      setTimeout(() => this.closeModal(), 1500);
+    }, () => {
+      this.emailStatus = 'error';
+      // Optionally close the modal after a short delay
+      setTimeout(() => this.closeModal(), 2000);
+    }
+    )
   }
 }
